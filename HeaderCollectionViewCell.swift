6651d4fdec371bb840691c6dfab36997f83c8793
bbyba <burte.bsn@gmail.com>
2023-11-20 16:06:12 +0900

    Button layout

---
+++ HeaderCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HeaderCollectionViewCell.swift
+//  Neobis_iOS_Calculating
+//
+//  Created by Burte Bayaraa on 2023.11.20.
+//
+
+import UIKit
+
+class HeaderCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Neobis_iOS_Calculating/CalculatorButtonModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorButtonModel.swift
+//  Neobis_iOS_Calculating
+//
+//  Created by Burte Bayaraa on 2023.11.20.
+//
+
+import Foundation
